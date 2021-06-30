3bef700ba6f03361b54a112aeedb718dee4c57f5
LimeEric <38434664+LimeEric@users.noreply.github.com>
2021-06-30 14:35:50 +0200

    df messenger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app-heroku';
-  ngOnInit(){
-    (function(d, m){
-      var kommunicateSettings = 
-          {"appId":"3212179e1fb948dda9f8512610ad97f7e","popupWidget":true,"automaticChatOpenOnNavigation":true};
-      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-      (window as any).kommunicate = m; m._globals = kommunicateSettings;
-  })(document, (window as any).kommunicate || {});
-  }
 }
